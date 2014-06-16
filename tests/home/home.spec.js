f6a5ed2aa2c15947242885bee116946ebd2f6cbf
Joel-Kornbluh <kornbluh@bdbdev.mygbiz.com>
2014-06-16 18:50:00 -0400

    added e2e test

--- tests/home/home.spec.js
+++ tests/home/home.spec.js
@@ -1,9 +1,25 @@
-describe('e2eTest', function(){
+describe('ccForm home page', function(){
 
     var baseUrl = 'http://127.0.0.1/ccForm/build/#/';
-    it('test', function(){
+
+    beforeEach(function(){
         browser.get(baseUrl + '');
+        p = protractor.getInstance();
     });
+
+    it('when CID is focused the card should flip', function(){
+
+        var cid = by.model('currentCC.cid');
+
+        expect(p.isElementPresent(cid)).toBe(true);
+
+        element(cid).sendKeys('4');
+
+
+
+    });
+
+
 
 
 });
